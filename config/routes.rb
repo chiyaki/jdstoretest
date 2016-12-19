ca84b6be955842594bb984a7f102df32c7454b0b
chiyaki <66chiyaki@gmail.com>
2016-12-20 06:20:07 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  resources :products
-  
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end  
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 end

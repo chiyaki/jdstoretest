84be2bc759274bde5bc21300edc70d78ca09b957
chiyaki <66chiyaki@gmail.com>
2016-12-20 10:06:56 +0800

    story5

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :products
+     resources :orders do
+       member do
+         post :cancel
+         post :ship
+         post :shipped
+         post :return
+       end
+     end
+  end
+
+  namespace :account do
+    resources :orders
   end
 
   resources :products do
@@ -14,8 +26,16 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       delete :clean
+      post :checkout
     end
   end
 
   resources :cart_items
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+      post :apply_to_cancel
+    end
+  end
 end
